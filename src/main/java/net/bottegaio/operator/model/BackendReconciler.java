23300ac345056a5fb01517c677498c471f8a46f1
Andrea Ambrosini - Rossonet s.c.a r.l <andrea.ambrosini@rossonet.org>
2022-06-08 01:10:17 +0200

    client funzionante

--- src/main/java/net/bottegaio/operator/model/BackendReconciler.java
+++ src/main/java/net/bottegaio/operator/model/BackendReconciler.java
@@ -1,15 +1,20 @@
 package net.bottegaio.operator.model;
 
+import java.util.logging.Logger;
+
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.javaoperatorsdk.operator.api.reconciler.Cleaner;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
+import io.javaoperatorsdk.operator.api.reconciler.DeleteControl;
+import io.javaoperatorsdk.operator.api.reconciler.ErrorStatusHandler;
+import io.javaoperatorsdk.operator.api.reconciler.ErrorStatusUpdateControl;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
 import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
 
 @ControllerConfiguration
-public class BackendReconciler implements Reconciler<Backend> {// , ErrorStatusHandler<Backend>, Cleaner<Backend>,
-																// EventSourceInitializer<Backend> {
-
+public class BackendReconciler implements Reconciler<Backend>, ErrorStatusHandler<Backend>, Cleaner<Backend> {
+	private static final Logger logger = Logger.getLogger(BackendReconciler.class.getName());
 	private final KubernetesClient client;
 
 	public BackendReconciler(final KubernetesClient client) {
@@ -17,9 +22,25 @@ public class BackendReconciler implements Reconciler<Backend> {// , ErrorStatusH
 	}
 
 	@Override
+	public DeleteControl cleanup(final Backend resource, final Context<Backend> context) {
+		// TODO Auto-generated method stub
+		logger.info("cleanup Backend " + resource + " -> " + context);
+		return DeleteControl.defaultDelete();
+	}
+
+	@Override
 	public UpdateControl<Backend> reconcile(final Backend resource, final Context<Backend> context) throws Exception {
 		// TODO Auto-generated method stub
+		logger.info("reconcile Backend " + resource + " -> " + context);
 		return UpdateControl.noUpdate();
+	}
+
+	@Override
+	public ErrorStatusUpdateControl<Backend> updateErrorStatus(final Backend resource, final Context<Backend> context,
+			final Exception e) {
+		// TODO Auto-generated method stub
+		logger.info("updateErrorStatus Backend " + resource + " -> " + context);
+		return ErrorStatusUpdateControl.noStatusUpdate();
 	}
 
 }
--- src/main/java/net/bottegaio/operator/model/WebappReconciler.java
+++ src/main/java/net/bottegaio/operator/model/WebappReconciler.java
@@ -1,15 +1,20 @@
 package net.bottegaio.operator.model;
 
+import java.util.logging.Logger;
+
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.javaoperatorsdk.operator.api.reconciler.Cleaner;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
+import io.javaoperatorsdk.operator.api.reconciler.DeleteControl;
+import io.javaoperatorsdk.operator.api.reconciler.ErrorStatusHandler;
+import io.javaoperatorsdk.operator.api.reconciler.ErrorStatusUpdateControl;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
 import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
 
 @ControllerConfiguration
-public class WebappReconciler implements Reconciler<Webapp> {// , ErrorStatusHandler<Webapp>, Cleaner<Webapp>,
-																// EventSourceInitializer<Webapp> {
-
+public class WebappReconciler implements Reconciler<Webapp>, ErrorStatusHandler<Webapp>, Cleaner<Webapp> {
+	private static final Logger logger = Logger.getLogger(WebappReconciler.class.getName());
 	private final KubernetesClient client;
 
 	public WebappReconciler(final KubernetesClient client) {
@@ -17,9 +22,25 @@ public class WebappReconciler implements Reconciler<Webapp> {// , ErrorStatusHan
 	}
 
 	@Override
+	public DeleteControl cleanup(final Webapp resource, final Context<Webapp> context) {
+		// TODO Auto-generated method stub
+		logger.info("cleanup Webapp " + resource + " -> " + context);
+		return DeleteControl.defaultDelete();
+	}
+
+	@Override
 	public UpdateControl<Webapp> reconcile(final Webapp resource, final Context<Webapp> context) throws Exception {
 		// TODO Auto-generated method stub
+		logger.info("reconcile Webapp " + resource + " -> " + context);
 		return UpdateControl.noUpdate();
+	}
+
+	@Override
+	public ErrorStatusUpdateControl<Webapp> updateErrorStatus(final Webapp resource, final Context<Webapp> context,
+			final Exception e) {
+		// TODO Auto-generated method stub
+		logger.info("updateErrorStatus Webapp " + resource + " -> " + context);
+		return ErrorStatusUpdateControl.noStatusUpdate();
 	}
 
 }
